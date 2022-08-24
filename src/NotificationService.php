9ec8b04b4d0130bdbfcf4a7db45510c60a1fd49a
Camilo <camiloomosqueraa@gmail.com>
2022-08-24 10:27:17 -0500

    Update repository new

---
+++ src/NotificationService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace TheTribe\NotificationMS;
+
+use GuzzleHttp\Client;
+
+final class NotificationService
+{
+    private $appURL;
+    private $notificationURL;
+    protected $client;
+
+    public function __construct(string $appURL, string $notificationURL)
+    {
+        $this->appURL = $appURL;
+        $this->notificationURL = $notificationURL;
+        $this->client = new Client();
+    }
+
+    public function save(array $content, array $params, string $action, int $categoryId, ?string $groupId = NULL)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Referer' => str_replace(['http://', 'https://'], '',  $this->appURL),
+            'Host' =>  str_replace(['http://', 'https://'], '', $this->appURL)
+        ];
+
+        $body = [
+            'content' => json_encode($content),
+            'params' => json_encode($params),
+            'action' => $action,
+            'category_id' => $categoryId,
+            'group_id' => $groupId,
+        ];
+
+        $response = $this->client->request('PUT', $this->notificationURL, [
+            'headers' => $headers,
+            'body' => json_encode($body)
+        ]);
+
+
+
+        return [
+            'code' => $response->getStatusCode(),
+            'body' => $response->getBody()
+        ];
+    }
+
+}
